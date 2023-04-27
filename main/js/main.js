87e396466d6b5ec3519b58450b9ebb28c4c3e29d
Edward Calles <ed.calles00@gmail.com>
2023-04-27 14:55:40 -0500

    fixed js file/fixed centering for tablet display

--- main/js/main.js
+++ main/js/main.js
@@ -1,3 +1,13 @@
+/*
 var api = "http://api.giphy.com/v1/gifs/search?";
 var apiKey = "&api_key=UP6732Wk58BBMmvGPzAAAyF8Hr07zfaj";
 var query = "&q=rainbow"
+*/
+$(document).ready(()=>{ 
+    $("#submit").click(()=>{
+        alert('clicked!')
+    })
+    $.ajax({
+        url: "http://api.giphy.com/v1/gifs/search?&api_key=UP6732Wk58BBMmvGPzAAAyF8Hr07zfaj"
+    });
+});
